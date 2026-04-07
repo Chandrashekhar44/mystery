97a05e073e3b774f636e7c93bd2c04f1cdf879cb
Chandra Shekhar <banothuchandu77@gmail.com>
2026-04-07 19:42:51 +0530

    refactor: streamline database connection logic and improve global state management

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,28 +1,33 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
-type ConnectionObject ={
-    isConnected? : number
-}
+type ConnectionObject = {
+  isConnected?: number;
+};
 
+const globalAny: any = globalThis;
+globalAny.mongoose = globalAny.mongoose || { conn: null, promise: null };
 
+async function dbConnect(): Promise<void> {
+  if (globalAny.mongoose.conn) {
+    console.log("Already connected to database");
+    return;
+  }
 
-const connection : ConnectionObject = {}
+  if (!globalAny.mongoose.promise) {
+    globalAny.mongoose.promise = mongoose
+      .connect(process.env.MONGODB_URI || "")
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
 
-async function dbConnect () : Promise<void>{
- if(connection.isConnected){
-     console.log("Already connected to database")
-     return
- }
-
- try{
-    const db = await mongoose.connect(process.env.MONGODB_URI || '')
-    connection.isConnected = db.connections[0].readyState
-    console.log("DB connected successfully")
- }catch(error){
-   console.log("Failed to connect DB ",error)
-
-   process.exit(1)
- }
+  try {
+    globalAny.mongoose.conn = await globalAny.mongoose.promise;
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log("Failed to connect DB", error);
+    process.exit(1);
+  }
 }
 
 export default dbConnect;
